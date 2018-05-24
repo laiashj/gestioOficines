b16f2a905ca01ceac8e7cc6c740df6e5dfd1b1ac
Eva <midiee2@gmail.com>
2018-05-24 15:27:13 +0200

    Drag n Drop

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -1,21 +1,56 @@
 import { NgModule } from '@angular/core';
+import {CdkTableModule} from '@angular/cdk/table';
 
 import {
+  MatAutocompleteModule,
   MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
+  MatButtonToggleModule,
   MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
   MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
 } from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+//import {MatInputModule} from '@angular/material/input';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
+//import {MatSelectModule} from '@angular/material/select';
+//import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+//import {MatCheckboxModule} from '@angular/material/checkbox';
+//import {MatIconModule} from '@angular/material/icon';
+//import {MatSnackBarModule} from '@angular/material/snack-bar';
+//import {MatListModule} from '@angular/material/list';
+//import {MatTableModule} from '@angular/material/table';
+//import {MatSortModule} from '@angular/material/sort';
+//import {MatPaginatorModule} from '@angular/material/paginator';
+//import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
+//         MatSortModule, MatTableModule } from "@angular/material";
+
 
 @NgModule({
   imports: [
@@ -25,14 +60,25 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatIconModule,
     MatCardModule,
 	MatFormFieldModule,
-	MatInputModule,
+	//MatInputModule,
 	MatDatepickerModule,
 	MatSelectModule,
 	MatNativeDateModule,
 	MatAutocompleteModule,
 	ReactiveFormsModule,
 	MatCheckboxModule,
-	MatSnackBarModule
+	MatSnackBarModule,
+	//MatTableModule,
+	MatListModule,
+	CdkTableModule,
+	//MatSortModule,
+	//MatPaginatorModule,
+	//MatPaginator,
+  //MatTableDataSource,
+  MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,  MatProgressBarModule, MatSliderModule,
+         MatSortModule, MatTableModule, MatGridListModule, MatRadioModule, MatSidenavModule, MatTabsModule,
+		 MatDividerModule, MatExpansionModule, MatRippleModule, MatStepperModule, MatTooltipModule,
+		  MatButtonToggleModule, MatChipsModule, MatDialogModule, MatSlideToggleModule
   ],
   exports: [
     MatButtonModule,
@@ -41,14 +87,24 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatIconModule,
     MatCardModule,
 	MatFormFieldModule,
-	MatInputModule,
+	//MatInputModule,
 	MatDatepickerModule,
 	MatSelectModule,
 	MatNativeDateModule,
 	MatAutocompleteModule,
 	ReactiveFormsModule,
 	MatCheckboxModule,
-	MatSnackBarModule
+	MatSnackBarModule,
+	MatListModule,
+	CdkTableModule, MatTooltipModule,
+	//MatTableModule,
+	//MatSortModule,
+	//MatPaginatorModule,
+	//MatPaginator,
+  //MatTableDataSource,
+  MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatGridListModule, MatProgressBarModule, MatSidenavModule, MatSliderModule,
+         MatSortModule, MatTableModule, MatDividerModule, MatExpansionModule, MatRadioModule, MatRippleModule, MatSlideToggleModule,
+		 MatButtonToggleModule, MatChipsModule, MatDialogModule, MatStepperModule, MatTabsModule
   ]
 })
 export class MaterialModule {}
--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,17 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import { UserService} from '../user.service';
 import { Router } from '@angular/router';
 import {HeaderComponent} from '../header/header.component';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import { DataSource } from '@angular/cdk/collections';
+import {startWith} from 'rxjs/operators/startWith';
+import { catchError, map, tap, switchMap, debounceTime, distinctUntilChanged, takeWhile, first } from 'rxjs/operators';
+import { TecnicService } from '../shared/tecnic/tecnic.service';
+import {MatTableDataSource} from '@angular/material';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit{
-	
-	constructor(private user:UserService){}
+export class UserComponent implements OnInit/*, AfterViewInit*/{
 
-	ngOnInit(){}
-}
+  
+  filteredOptions:any;
+  
+	
+  
+	constructor(private user:UserService, private dragulaService: DragulaService, public tecnicService:TecnicService){
+	dragulaService.setOptions('bag-task1', {
+      removeOnSpill: true
+    });
+	
+	}
+	
+	ngOnInit(){
+	this.tecnicService.getAll().subscribe(
+	data => {
+	this.filteredOptions= data;
+	//this.dataSource1.data=data;});
+	}
+	//dataSource1=new MatTableDataSource<any>(ELEMENT_DATA);
+	//displayedColumns1=['nomCognom'];
+	
+	//@ViewChild(MatPaginator) paginator: MatPaginator;
+	//@ViewChild(MatSort) sort: MatSort;
+	/*ngAfterViewInit(){
+		//this.dataSource1.paginator=this.paginator;
+		//this.dataSource1.sort=this.sort;
+	}
+	applyFilter(filterValue:string){
+		filterValue=filterValue.trim();
+		filterValue=filterValue.toLowerCase();
+		this.dataSource1.filterValue=filterValue;
+		
+	}*/
+	
+	
+  }
+//const ELEMENT_DATA: Element[]=[];
+	
+  
+
