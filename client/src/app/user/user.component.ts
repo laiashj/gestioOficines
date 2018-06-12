69428e858e0da9dac7bcc9a329307a637d4c6388
Eva <midiee2@gmail.com>
2018-06-12 08:39:52 +0200

    Revert "Printeja els tecnics Assignats"
    This reverts commit dd5aba9b034646b04475d81b06b6a30db1bc7be4.

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -23,14 +23,13 @@ import { CommonModule } from '@angular/common';
 export class UserComponent implements OnInit/*, AfterViewInit*/{
  
 	filteredOptions:any;
-	assignats: any;
+	assignats: Array<Object>;
 	htmlStr:string='<strong>The Tortoise</strong>';
 	tec={
 		nomCognom:"Laura Castillo",
 		dataAlta:"",
 		color: "purple"
 	};
-	tecnic1:string;
 	
 	tec1:string= "<div [style.background-color]=\"tec.color\" > {{tec.nomCognom}}</div>";
 	llocs = {
@@ -41,8 +40,8 @@ export class UserComponent implements OnInit/*, AfterViewInit*/{
 		l5: "", 
 		l6: ""
 	};
-	//i : int = 0;
-	//j : int = 0;
+	i : int = 0;
+	j : int = 0;
 	
 	//130 taules
 	
@@ -57,20 +56,19 @@ export class UserComponent implements OnInit/*, AfterViewInit*/{
 			}
 		);
 		
-		this.tecnicService.getTecnicsEstat('Assignat').subscribe(
+		this.tecnicService.getTecnicsEstat('assignat').subscribe(
 			data => {
 				this.assignats= data;
 			}
 		);
-		//this.tecnic1="{{assignats && assignats[0].nomCognom}}";
-		/*this.llocs.l1 = assignats[0];
+		
+		this.llocs.l1 = assignats[0];
 		this.llocs.l2 = assignats[1];
 		this.llocs.l3 = assignats[2];
 		this.llocs.l4 = assignats[3];
 		this.llocs.l5 = assignats[4];
-		this.llocs.l6 = assignats[5];*/
+		this.llocs.l6 = assignats[5];
 	}
-	
 	
 	
 	/*public visualitzaTec(){
